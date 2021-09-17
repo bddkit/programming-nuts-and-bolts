800a6a907c8409f76db52bcb58bc7774ad60bae6
bddkit <azad_ce@yahoo.com>
2021-09-17 18:02:39 +0200

    Initial Commit

---
+++ CSharpBasics/1GettingStarted/GettingStarted/GettingStarted.101/Operators.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GettingStarted.OneZeroOne
+{
+	/// <summary>
+	/// Operators are used to perform operations on variables and values.
+	/// </summary>
+	public static class Operators
+	{
+		public static void CommonOperatos() //it is nested method call i.e. calling one method inside other
+		{
+			Addition(); 
+			Subtraction();
+			Multiplication();
+			Division();
+			Modulus();
+			Increment();
+			Decrement();
+		}
+
+		private static void Addition() //If you don't understand 'private', it is coming later
+		{
+			int x = 7;
+			int y = 1;
+			Console.WriteLine("Addition Operator: {0}", x + y);
+		}
+
+		private static void Subtraction()
+		{
+			int x = 15;
+			int y = 13;
+			Console.WriteLine("Subtraction Operator: {0}", x - y);
+		}
+
+		private static void Multiplication()
+		{
+			int x = 8;
+			int y = 9;
+			Console.WriteLine("Multiplication Operator: {0}", x * y);
+		}
+
+		private static void Division()
+		{
+			int x = 18;
+			int y = 2;
+			Console.WriteLine("Division Operator: {0}", x / y);
+		}
+
+		private static void Modulus() //Returns the division remainder
+		{
+			int x = 67;
+			int y = 9;
+			Console.WriteLine("Modulus Operator: {0}", x % y);
+		}
+
+		private static void Increment() //Increases the value of a variable by 1
+		{
+			int x = 8;
+			x++;
+			Console.WriteLine("Increment Operator: {0}", x);
+		}
+
+		private static void Decrement() //Decreases the value of a variable by 1
+		{
+			int x = 8;
+			x--;
+			Console.WriteLine("Decrement Operator: {0}", x);
+		}
+	}
+}
---
+++ CSharpBasics/1GettingStarted/GettingStarted/GettingStarted.101/Program.cs
@@ -0,0 +1,24 @@
+﻿using System; //we can use classes from the System namespace.
+
+namespace GettingStarted.OneZeroOne //used to organize your code, and it is a container for classes and other namespaces
+{ //The curly braces {} marks the beginning and the end of a block of code.
+	public class Program //class is a container for data and methods, which brings functionality to your program
+	{
+		public static void Main(string[] args) //starting point of a program
+		{
+			Console.WriteLine("Welcome to Getting Started C# 101!"); //output/print text
+			Variables.PrintAllTypesOfVariables(); //different type of variables
+			CommentInCode.PrintTypesOfCommentsInCode(); //way of making comment in code
+			TypeCasting.ImplicitTypeCasting(); //implicit type casting
+			TypeCasting.ExplicitTypeCasting(); //explict type casting
+			TypeCasting.BuiltInTypeConversionMethods(); //built in methods for type casting
+
+			//Uncomment the code below if you want to test 
+			//UserInput.ReadInput();
+
+			Operators.CommonOperatos();
+		}
+	}
+
+	//If you don't understand 'public' 'static' 'void', no problem it will come later.
+}
