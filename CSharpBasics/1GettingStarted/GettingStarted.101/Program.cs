f465c10446202edfae31265b7f4177f9e4f17503
bddkit <azad_ce@yahoo.com>
2021-10-25 15:20:18 +0200

    Done with gettig started part

--- CSharpBasics/1GettingStarted/GettingStarted.101/Program.cs
+++ CSharpBasics/1GettingStarted/GettingStarted.101/Program.cs
@@ -11,6 +11,8 @@ namespace GettingStarted.OneZeroOne //used to organize your code, and it is a co
 		public static void Main(string[] args) //starting point of a program
 		{
 			Console.WriteLine("###### Welcome to Getting Started C# 101 #####"); //output/print text
+			Method(); //Calling a method 
+			Method("Hello"); //Method that takes parameter
 
 			Console.WriteLine("***** Variables in C# *****\n"); //\n print the new line at the end
 			Variables.PrintAllTypesOfVariables(); //different type of variables
@@ -57,6 +59,40 @@ namespace GettingStarted.OneZeroOne //used to organize your code, and it is a co
 			Console.WriteLine("***** Conditions in C# *****\n");
 			Conditions.Learn();
 			Console.WriteLine("****************************\n");
+
+			Console.WriteLine("***** Loops in C# *****\n");
+			Loops.EntryControlledLoops();
+			Loops.ExitControlledLoops();
+			Loops.LoopWithBreak();
+			Loops.LoopWithContinue();
+			//Uncomment the code below if you want to test InfiniteLoop()
+			//Loops.InfiniteLoop();
+			Console.WriteLine("***********************\n");
+
+			Console.WriteLine("***** Array in C# *****\n");
+			Array.Learn();
+			Console.WriteLine("***********************\n");
+		}
+
+		/// <summary>
+		/// A method is a block of code which only runs when it is called.
+		/// Methods are used to perform certain actions, and they are also known as functions.
+		/// Methods are used reuse code: define the code once, and use it many times.
+		/// </summary>
+		public static void Method() //Method name is 'Method' here
+		{
+			Console.WriteLine("This is a method");
+		}
+
+		/// <summary>
+		/// Data can be passed as parameter(s) into a method.
+		/// </summary>
+		/// <param name="parameter"></param>
+		public static void Method(string parameter)
+		{
+			//Method overloading: multiple methods can have the same name with different parameters.
+			//The other method (Method()) does not take any parameter and this method takes a single parameter.
+			Console.WriteLine("This is a overloaded method that takes one parameter. Parameter is: {0}", parameter);
 		}
 	}
 
